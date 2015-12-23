3e259f0f222ae2a1107224de3bbad1b90f0a5913
jy <jyhwang@cowellchina.com>
2015-12-23 10:15:54 +0800

    initial

---
+++ test_2.cpp
@@ -0,0 +1,17 @@
+#include< iostream >
+
+using namespace std;
+
+int main()
+{
+	
+	cout << "Hello Hello" << endl;
+	cout << "Hello world!" << endl;
+	cout << "Hi Hi " << endl;
+	cout << "Welcome to the Hell" << endl;
+	cout << "Bye~" << endl;
+
+
+	return 0;
+}
+
