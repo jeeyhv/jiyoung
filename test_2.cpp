ea059b31a2cfe1f9cacec8ffecde5289999b1fb3
jiyoung <jyhwang@cowellchina.com>
2015-12-24 16:29:45 +0800

    modified by branch_1

--- test_2.cpp
+++ test_2.cpp
@@ -8,6 +8,7 @@ int main()
 	cout << "Hello Hello" << endl;
 	cout << "Hello world!" << endl;
 	cout << "Hi Hi " << endl;
+	cout << "===========" << endl;
 	cout << "Welcome to the Hell" << endl;
 	cout << "HiHiHi" << endl;
 	cout << "Bye~" << endl;
